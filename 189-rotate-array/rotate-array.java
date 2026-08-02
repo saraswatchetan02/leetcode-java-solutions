79da84983ab6d813ebcfe6fd9fae1d777b02f159
Chetan saraswat <saraswatchetan90@gmail.com>
2026-08-02 18:09:56 +0530

    Time: 2 ms (19.02%) | Memory: 61.2 MB (56.15%) - LeetSync

--- 189-rotate-array/rotate-array.java
+++ 189-rotate-array/rotate-array.java
@@ -1,13 +1,36 @@
 class Solution {
     public void rotate(int[] nums, int k) {
-       ArrayList<Integer> list=new ArrayList<>();
-       for(int i=0;i<nums.length;i++){
-        list.add(nums[i]);
+       int n=nums.length;
+       k=k%n;
+       if(k==0)
+       return;
+       int i=0,j=n-1;
+       while(i<j){
+        int temp=nums[i];
+        nums[i]=nums[j];
+        nums[j]=temp;
+        i++;
+        j--;
        }
-       Collections.rotate(list,k);
-       for(int i=0;i<nums.length;i++){
-        nums[i]=list.get(i);
+       i=0;
+       j=k-1;
+       while(i<j){
+        int temp=nums[i];
+        nums[i]=nums[j];
+        nums[j]=temp;
+        i++;
+        j--;
+       }
+       i=k;
+       j=n-1;
+       while(i<j){
+        int temp=nums[i];
+        nums[i]=nums[j];
+        nums[j]=temp;
+        i++;
+        j--;
+       }
        }
         }
       
-    }
+    
