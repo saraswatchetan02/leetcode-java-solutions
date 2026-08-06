b4cc7e4af766e40994943a166389e25404d818d2
Chetan saraswat <saraswatchetan90@gmail.com>
2026-08-06 21:53:35 +0530

    Time: 55 ms (22.51%) | Memory: 47.7 MB (67.66%) - LeetSync

---
+++ 1031-add-to-array-form-of-integer/add-to-array-form-of-integer.java
@@ -0,0 +1,14 @@
+class Solution {
+    public List<Integer> addToArrayForm(int[] num, int k) {
+     ArrayList<Integer> list=new ArrayList<>();
+       for(int i=num.length-1;i>=0;i--){
+        list.add(0,(num[i]+k)%10);
+        k=(num[i]+k)/10;
+       } 
+       while(k>0){
+        list.add(0,k%10);
+        k/=10;
+       }
+       return list;
+    }
+}
