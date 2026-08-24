2c89a631fc9b1ac67a155dadaed80e38f36cf812
Chetan saraswat <saraswatchetan90@gmail.com>
2026-08-24 13:56:39 +0530

    Time: 2 ms (99.79%) | Memory: 69.6 MB (63.33%) - LeetSync

---
+++ 643-maximum-average-subarray-i/maximum-average-subarray-i.java
@@ -0,0 +1,17 @@
+class Solution {
+    public double findMaxAverage(int[] nums, int k) {
+      int left=0;
+      int sum=0;
+      for(int i=0;i<k;i++){
+        sum+=nums[i];
+      }  
+      int max=sum;
+      for(int i=k;i<nums.length;i++){
+        sum-=nums[left++];
+        sum+=nums[i];
+        max=Math.max(max,sum);
+      }
+      double avg = (double) max / k;
+      return avg;
+    }
+}
