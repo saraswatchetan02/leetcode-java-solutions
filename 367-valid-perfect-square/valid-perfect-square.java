43ce49c60a5f89db8f6fc5b9d8dbeea9b02367f8
Chetan saraswat <saraswatchetan90@gmail.com>
2026-09-02 21:24:14 +0530

    Time: 0 ms (100.00%) | Memory: 41.6 MB (97.32%) - LeetSync

---
+++ 367-valid-perfect-square/valid-perfect-square.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isPerfectSquare(int num) {
+       if(num==1){
+        return true;
+       }
+       long left=1;
+       long right=num/2;
+       while(left<=right){
+        long mid=left+(right-left)/2;
+        if(mid*mid==num){
+            return true;
+        }
+        else if(mid*mid>num){
+            right=mid-1;
+        }
+        else{
+            left=mid+1;
+        }
+       }
+       return false;
+    }
+}
