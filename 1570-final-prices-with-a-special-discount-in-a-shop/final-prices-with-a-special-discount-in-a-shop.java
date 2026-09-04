336a3c6826c6c2b4f8f84716918a1759f806a30f
Chetan saraswat <saraswatchetan90@gmail.com>
2026-09-04 12:45:43 +0530

    Time: 2 ms (72.37%) | Memory: 45.3 MB (58.64%) - LeetSync

---
+++ 1570-final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] finalPrices(int[] prices) {
+        int res[] = new int[prices.length];
+        for (int i = 0; i < prices.length; i++) {
+            res[i] = prices[i];
+            for (int j = i + 1; j < prices.length; j++) {
+                if (j > i && prices[j] <= prices[i]) {
+                    res[i] = prices[i] - prices[j];
+                    break;
+                }
+            }
+
+        }
+        return res;
+    }
+}
