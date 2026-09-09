7cf1457784baeb99cb3df072dff05f002557a1e8
Chetan saraswat <saraswatchetan90@gmail.com>
2026-09-09 20:52:51 +0530

    Time: 5 ms (82.42%) | Memory: 46 MB (32.84%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+      int rem = 0;
+        int rev = 0;
+        int original = x;
+        while (x > 0) {
+            rem = x % 10;
+            rev = rev * 10 + rem;
+            x = x / 10;
+        }
+        if (original == rev) {
+            return true;
+        } else {
+            return false;
+        }  
+    }
+}
