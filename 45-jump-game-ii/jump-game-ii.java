01da13fa1c0f3d07164d44bc932cc2441a98c0e2
Chetan saraswat <saraswatchetan90@gmail.com>
2026-09-13 18:12:16 +0530

    Time: 1 ms (99.79%) | Memory: 47.2 MB (57.71%) - LeetSync

---
+++ 45-jump-game-ii/jump-game-ii.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int jump(int[] nums) {
+        int n=nums.length;
+       int jumps=0;
+       int l=0,r=0;
+       while(r < n-1){
+        int far=0;
+        for(int i=l;i<=r;i++){
+            far=Math.max(far,i+nums[i]);
+        }
+        l=r+1;
+        r=far;
+        jumps++;
+       } 
+       return jumps;
+    }
+}
