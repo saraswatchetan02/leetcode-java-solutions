64d3dfd23f67d1b4c6dc5318434680d78e611f38
Chetan saraswat <saraswatchetan90@gmail.com>
2026-09-14 22:26:38 +0530

    Time: 36 ms (28.22%) | Memory: 59.6 MB (20.00%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,18 +1,35 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         Set<List<Integer>> uniqset = new HashSet<>();
+        Arrays.sort(nums);
         for (int i = 0; i < nums.length; i++) {
-            Set<Integer> set = new HashSet<>();
-            for (int j = i + 1; j < nums.length; j++) {
-                int k = -(nums[i] + nums[j]);
-                if (set.contains(k)) {
-                    List<Integer> temp = Arrays.asList(nums[i], nums[j], k);
-                    temp.sort(null);
-                    uniqset.add(temp);
+           if(i>0 && nums[i]==nums[i-1]){
+            continue;
+           }
+           int left=i+1;
+           int right=nums.length-1;
+           while(left<right){
+            int sum=nums[i]+nums[left]+nums[right];
+            if(sum==0){
+                uniqset.add(Arrays.asList(nums[i],nums[left],nums[right]));
+                while(left<right && nums[left]==nums[left+1]){
+                    left++;
                 }
-                set.add(nums[j]);
+                while(left<right && nums[right]==nums[right-1]){
+                    right--;
+                }
+                left++;
+                right--;
             }
-        }
-        return new ArrayList<>(uniqset);
-    }
-}
+            else if(sum<0){
+                left++;
+            }
+            else{
+                right--;
+            }
+           }
+                }
+             return new ArrayList<>(uniqset);
+            }
+            }
+        
